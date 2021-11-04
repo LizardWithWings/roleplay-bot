e98c2d67e021c0d4f71ba1a272811d05af9b2e4f
robloxbloxxer50 <braydenisawesome.282@gmail.com>
2021-11-04 11:36:56 -0700

    Updated bot.js and package.json, Created bot-dev.js
    Created bot-dev.js instead of using a boolean to determine weather to use public mode or dev mode as it's annoying to have to switch the value to true everytime i commit.

--- bot.js
+++ bot.js
@@ -1,43 +1,26 @@
 require("dotenv").config()
 const slasho = require("discord-slasho")
 const { CommandInteraction } = require("discord.js")
-var token
 
 //Command Handler Script
 const cmds = require("./cmds/handler.js")
 
-//SET TO FALSE TO ENABLE DEV MODE - Bloxxer Dumbass Countermeasure 1
-const launchToPublic = true
-
-//Setting which token to launch from.
-if (launchToPublic) {
-  console.warn("LAUNCHING IN PUBLIC MODE")
-  token = process.env.DISCORD_TOKEN
-} else {
-  console.warn("LAUNCHING IN DEV MODE")
-  token = process.env.DISCORD_TOKEN_DEV
-}
-
 
 const bot = new slasho.App({
-    token: token,
-    devGuild: "714337883116535868",
-    intents: ["GUILDS"],
-    commands: [
-      cmds.makechar, 
-      cmds.editchar, 
-      cmds.deletechar,
-      cmds.viewchar,
-      cmds.listchars,
-      cmds.info,
-      //cmds.test
-      ]
+  token: process.env.DISCORD_TOKEN,
+  devGuild: "714337883116535868",
+  intents: ["GUILDS"],
+  commands: [
+    cmds.makechar,
+    cmds.editchar,
+    cmds.deletechar,
+    cmds.viewchar,
+    cmds.listchars,
+    cmds.info,
+    //cmds.test
+  ]
 })
 
 bot.launch().then(() => {
-    if (launchToPublic) {
-        bot.production()
-    } else {
-        bot.dev()
-    }
+  bot.production()
 })
